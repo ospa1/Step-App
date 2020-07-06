7cfc67ff9c0f40037e83e7369abecfe34ea92b26
ospa1 <oscarkil09@yahoo.com>
2020-07-06 01:03:35 -0500

    added a stats page

--- StepApp/app/src/main/java/com/example/stepapp/Stats.java
+++ StepApp/app/src/main/java/com/example/stepapp/Stats.java
@@ -1,7 +1,19 @@
 package com.example.stepapp;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 public class Stats extends AppCompatActivity {
-    //TODO add everything
+    //TODO call the database and get information
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.stats_layout);
+
+        // optional if intent stored information
+        //Intent intent = getIntent();
+        //String value = intent.getStringExtra("key"); //if it's a string you stored.
+    }
 }
